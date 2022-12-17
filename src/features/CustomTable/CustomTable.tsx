d5dbbb7dbfd38012c3d7f47a6993abbc05489847
ArhDemetriy <ArhDemetriy@GMail.com>
2022-12-17 10:16:27 +0800

    dev table

--- src/features/CustomTable/CustomTable.tsx
+++ src/features/CustomTable/CustomTable.tsx
@@ -1,19 +1,49 @@
 import { FC } from 'react';
+import { ColumnCaptions, TableCaption } from './TableConstants';
 import styles from './CustomTable.module.scss';
 
 interface CustomTableProps{
-    caption?: string
+    caption?: boolean
 }
 
 export const CustomTable: FC<CustomTableProps> = p => {
 
     return <table className={styles['table']}>
-        {!p.caption?.length ? null
-            : <caption children={p.caption} />}
+        {!p.caption ? null
+            : <caption className={styles['table__caption']}>{TableCaption.value}</caption>}
         <colgroup>
-            <col className={styles['table__first_col']}/>
-            <col span={2} />
+            <col className={styles['table__ui_col']}/>
+            <col className={styles['table__title_col']}/>
         </colgroup>
-        <thead></thead>
+        <thead className={styles['table__head']}>
+            <tr>
+                <th scope="col">{ColumnCaptions.ui}</th>
+                <th scope="col">{ColumnCaptions.rowName}</th>
+                <th scope="col">{ColumnCaptions.salary}</th>
+                <th scope="col">{ColumnCaptions.equipmentCosts}</th>
+                <th scope="col">{ColumnCaptions.overheads}</th>
+                <th scope="col">{ColumnCaptions.estimatedProfit}</th>
+            </tr>
+        </thead>
+        <tbody className={styles['table__body']}>
+            <tr>
+                <td>item0</td>
+                <td>item1</td>
+                <td>item2</td>
+                <td>item3</td>
+                <td>item4</td>
+                <td>item5</td>
+                <td>item6</td>
+            </tr>
+            <tr>
+                <td>item0</td>
+                <td>item1</td>
+                <td>item2</td>
+                <td>item3</td>
+                <td>item4</td>
+                <td>item5</td>
+                <td>item6</td>
+            </tr>
+        </tbody>
     </table>
 }
---
+++ src/features/CustomTable/TableConstants.ts
@@ -0,0 +1,11 @@
+export const enum TableCaption {
+    value = 'Строительно-монтажные работы'
+}
+export const enum ColumnCaptions {
+    ui = 'Уровень',
+    rowName = 'Наименование работ',
+    salary = 'Основная з/п',
+    equipmentCosts = 'Оборудование',
+    overheads = 'Накладные расходы',
+    estimatedProfit = 'Сметная прибыль',
+}
