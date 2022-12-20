23ece801d01e9c676b0fd7f23fadc51e8f120d6a
ArhDemetriy <ArhDemetriy@GMail.com>
2022-12-20 13:34:35 +0800

    minor ref

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import styles from './App.module.scss';
 import { CustomTable } from './features/CustomTable/CustomTable';
 // import { Counter } from './features/counter/Counter';
-import { CustomTreeView } from './features/treeVIew/CustomTreeView';
+// import { CustomTreeView } from './features/treeVIew/CustomTreeView';
 // import { CustomSelect } from './features/SelectorTest/CustomSelect';
 
 function App() {
@@ -9,7 +9,7 @@ function App() {
     <div className="App">
       <div className={styles['App__columns']}>
         <CustomTable />
-        <CustomTreeView />
+        {/* <CustomTreeView /> */}
         {/* <Counter /> */}
         {/* <CustomSelect /> */}
       </div>
